9e066aac307f190948f530ea24dcdb0ac86cf093
BNR <nrajithabhogadi@gmail.com>
2026-05-26 16:28:29 -0500

    Initial Commit

---
+++ MyTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTableView
+//
+//  Created by Naga Rajitha Bhogadi on 5/26/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
